a2d4e3872d894769ddc0b4862d923e12ec5a5e6f
Miguel1357Ruiz <62457456+Miguel1357Ruiz@users.noreply.github.com>
2020-03-21 19:33:40 -0600

    Add files via upload

---
+++ toPow.js
@@ -0,0 +1,11 @@
+function pow(x, y){
+    var i=0, a=1;
+    for(i=0; i<y; i++){
+	a=a*x;
+    }
+	return a;
+}
+
+print(pow(5, 4));
+
+
---
+++ toRadian.js
@@ -0,0 +1,7 @@
+function radian2degree(radian){
+return radian*(180/Math.PI)
+}
+
+print(radian2degree(Math.PI));
+print(radian2gree(2*Math.PI));
+
